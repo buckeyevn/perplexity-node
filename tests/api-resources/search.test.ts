ecf2480051306209be0d09da9b630b27e5b4ffa8
Minh Pham <williamsjason2591@gmail.com>
2026-03-12 03:00:01 -0700

    Add tests/api-resources/search.test.ts

---
+++ tests/api-resources/search.test.ts
@@ -0,0 +1,42 @@
+// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+import Perplexity from '@perplexity-ai/perplexity_ai';
+
+const client = new Perplexity({
+  apiKey: 'My API Key',
+  baseURL: process.env['TEST_API_BASE_URL'] ?? 'http://127.0.0.1:4010',
+});
+
+describe('resource search', () => {
+  // Mock server tests are disabled
+  test.skip('create: only required params', async () => {
+    const responsePromise = client.search.create({ query: 'string' });
+    const rawResponse = await responsePromise.asResponse();
+    expect(rawResponse).toBeInstanceOf(Response);
+    const response = await responsePromise;
+    expect(response).not.toBeInstanceOf(Response);
+    const dataAndResponse = await responsePromise.withResponse();
+    expect(dataAndResponse.data).toBe(response);
+    expect(dataAndResponse.response).toBe(rawResponse);
+  });
+
+  // Mock server tests are disabled
+  test.skip('create: required and optional params', async () => {
+    const response = await client.search.create({
+      query: 'string',
+      country: 'country',
+      display_server_time: true,
+      last_updated_after_filter: 'last_updated_after_filter',
+      last_updated_before_filter: 'last_updated_before_filter',
+      max_results: 0,
+      max_tokens: 0,
+      max_tokens_per_page: 0,
+      search_after_date_filter: 'search_after_date_filter',
+      search_before_date_filter: 'search_before_date_filter',
+      search_domain_filter: ['string'],
+      search_language_filter: ['string'],
+      search_mode: 'web',
+      search_recency_filter: 'hour',
+    });
+  });
+});
